de0aa3b5bdc0bda219e81037cb5947c182751859
Gladston Joseph <gladstonjoseph@gladstons-macbook-air.local>
2019-02-20 16:05:15 +0530

    Initializing the Project

--- AudioPlayer/ViewController.swift
+++ AudioPlayer/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,  selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+        timer.invalidate()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        timer.invalidate()
+        player.pause()
+        scrubber.value = scrubber.minimumValue
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        } catch {
+            // Process any errors
+        }
+    }
+    
+    @IBAction func volumeChanged(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func scrubberMoved(_ sender: Any) {
+        player.currentTime = TimeInterval(scrubber.value)
+    }
+    
+    @IBOutlet weak var scrubber: UISlider!
+    
+    var player = AVAudioPlayer()
+    let audioPath = Bundle.main.path(forResource: "sheep", ofType: "mp3")
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            scrubber.maximumValue = Float(player.duration)
+        } catch {
+            // Process any errors
+        }
     }
-
-
+    @objc func updateScrubber() {
+        scrubber.value = Float(player.currentTime)
+    }
 }
-
